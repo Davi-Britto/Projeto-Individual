763afeab5219cf60697b932ed70b1d2ce1271386
Davi-Britto <davi.britto@sptech.school>
2023-05-22 20:51:51 -0300

    Tela de Avaliação + conexão com o Banco de Dados

---
+++ site/src/controllers/avisoController.js
@@ -0,0 +1,151 @@
+var avisoModel = require("../models/avisoModel");
+
+function testar(req, res) {
+    console.log("ENTRAMOS NO avisoController");
+    res.send("ENTRAMOS NO AVISO CONTROLLER");
+}
+
+function listar(req, res) {
+    avisoModel.listar().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function listarPorUsuario(req, res) {
+    var idUsuario = req.params.idUsuario;
+
+    avisoModel.listarPorUsuario(idUsuario)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "Houve um erro ao buscar os avisos: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function pesquisarDescricao(req, res) {
+    var descricao = req.params.descricao;
+
+    avisoModel.pesquisarDescricao(descricao)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function publicar(req, res) {
+    var idUsuario = req.params.idUsuario;
+    var qtdEstrelas = req.body.qtdEstrelas;
+    var descricao = req.body.descricao;
+
+
+    if (idUsuario == undefined) {
+        res.status(400).send("O id do usuário está indefinido!");
+    } else if (qtdEstrelas == undefined) {
+        res.status(400).send("A quantidade de estrelas está indefinido!");
+    } else if (descricao == undefined) {
+        res.status(403).send("A descrição está indefinido!");
+    }
+    else {
+        avisoModel.listarPorUsuario(idUsuario).then((resultado) => {
+            if (resultado.length > 0) {
+                res.status(400).send("O usuário já fez uma avaliação"); //mexer
+            } else {
+                avisoModel.publicar(idUsuario, qtdEstrelas, descricao)
+                    .then(
+                        function (resultado) {
+                            res.json(resultado);
+                        }
+                    )
+                    .catch(
+                        function (erro) {
+                            console.log(erro);
+                            console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                            res.status(500).json(erro.sqlMessage);
+                        }
+                    );
+            }
+        })
+
+    }
+}
+
+function editar(req, res) {
+    var novaDescricao = req.body.descricao;
+    var idAviso = req.params.idAviso;
+
+    avisoModel.editar(novaDescricao, idAviso)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+
+}
+
+function deletar(req, res) {
+    var idAviso = req.params.idAviso;
+
+    avisoModel.deletar(idAviso)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao deletar o post: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+module.exports = {
+    testar,
+    listar,
+    listarPorUsuario,
+    pesquisarDescricao,
+    publicar,
+    editar,
+    deletar
+}
---
+++ site/src/models/avisoModel.js
@@ -0,0 +1,83 @@
+var database = require("../database/config");
+
+function listar() {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+    select * from avaliacao join usuario
+    on id = fkUsuario
+        join estrela
+            on idEstrela = fkEstrela;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function pesquisarDescricao(texto) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
+    var instrucao = `
+        SELECT 
+            a.id AS idAviso,
+            a.titulo,
+            a.descricao,
+            a.fk_usuario,
+            u.id AS idUsuario,
+            u.nome,
+            u.email,
+            u.senha
+        FROM aviso a
+            INNER JOIN usuario u
+                ON a.fk_usuario = u.id
+        WHERE a.descricao LIKE '${texto}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function listarPorUsuario(idUsuario) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
+    var instrucao = `
+        select * from avaliacao join usuario
+        on id = fkUsuario
+            join estrela
+                on idEstrela = fkEstrela
+                    WHERE usuario.id = ${idUsuario};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function publicar(idUsuario, qtdEstrelas, descricao) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", idUsuario, qtdEstrelas, descricao);
+    var instrucao = `
+        INSERT INTO avaliacao (fkUsuario, fkEstrela, descricao) VALUES ('${idUsuario}', '${qtdEstrelas}', '${descricao}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function editar(novaDescricao, idAviso) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novaDescricao, idAviso);
+    var instrucao = `
+        UPDATE aviso SET descricao = '${novaDescricao}' WHERE id = ${idAviso};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function deletar(idAviso) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():", idAviso);
+    var instrucao = `
+        DELETE FROM aviso WHERE id = ${idAviso};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    listar,
+    listarPorUsuario,
+    pesquisarDescricao,
+    publicar,
+    editar,
+    deletar
+}
